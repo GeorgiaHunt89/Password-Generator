168848171994b51ab8cb43b0014018f495e437b5
Georgia Hunt <georgialhunt89@gmail.com>
2021-03-22 13:48:41 +1030

    Function to ask for lowercase characters

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-// Define Uppercase, Lowercase, numbers and Special icon variables 
+// Uppercase, Lowercase, numbers and Special icon variables 
 
-var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var special = '!@#$%^&*';
 var numbers = '1234567890';
 
 var password = ['#password'];
 
-
-
 // Function to create random #password input
 
 function writePassword() {
-  var passwordlength = '#password';
+  var passwordLength = '#password';
 
   // Function to ask for password length 
-  While (passwordlength >= 8 && passwordlength <=128){
-    passwordlength = prompt ("Please enter length of password, (must be between 8 and 128 characters)") 
-      if (passwordlength === undefined){
-        passwordlength = prompt ("Incorrect character length, please enter a number between 8 and 128")
+  Const (passwordLength >= 8 && passwordLength <=128);{
+    passwordLength = window.prompt ('Please enter length of password, (must be between 8 and 128 characters)'); 
+      if (passwordLength === undefined){
+        alert ('Incorrect character length, please enter a number between 8 and 128');
       }
+  }
+
+  // Function to ask for lowercase characters
+  if (passwordLength){
+    if (window.prompt ('Would you like to include lowercase characters?') === true)
+        lowerCaseFunction == true
   }
   
   var password = generatePassword();
@@ -32,4 +36,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-ar generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
