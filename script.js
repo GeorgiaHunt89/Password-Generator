f9b196bfd328fa5b9de13cfe284224e138343a56
Georgia Hunt <georgialhunt89@gmail.com>
2021-03-22 13:03:31 +1030

    Function for password length added to JS

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-// Define Uppercase, Lowercase, numbers and Special icon variables //
+// Define Uppercase, Lowercase, numbers and Special icon variables 
 
 var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var special = '!@#$%^&*';
 var numbers = '1234567890';
 
-var password = [''];
+var password = ['#password'];
 
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
+// Function to create random #password input
+
 function writePassword() {
+  var passwordlength = '#password';
+
+  // Function to ask for password length 
+  if(passwordlength >= 8 && passwordlength <=128){
+    alert ("Please enter length of password, must be between 8 and 128 characters")
+  }
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -20,3 +28,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+ar generateBtn = document.querySelector("#generate");
