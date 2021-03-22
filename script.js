f857fd9a693090e6991df0aad227b587684006b2
Georgia Hunt <georgialhunt89@gmail.com>
2021-03-22 12:27:36 +1030

    Defined variables for password

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// Assignment Code
+// Define Uppercase, Lowercase, numbers and Special icon variables //
+
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var special = '!@#$%^&*';
+var numnbers = '1234567890';
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
