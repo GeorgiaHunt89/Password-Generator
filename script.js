bb8be5bea4648e40342af3861b24f716570f0142
Georgia Hunt <georgialhunt89@gmail.com>
2021-03-22 20:58:53 +1030

    Function to list allChars and writePassword added

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Uppercase, Lowercase, numbers and Special icon variables 
 
-var generateBtn = document.querySelector('#generate')
-var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 var special = '!@#$%^&*';
 var numbers = '1234567890';
 var passwordLength;
@@ -11,89 +10,76 @@ var confirmUpperCase;
 var confirmNumbersChar;
 var confirmSpecialChar;
 var password = "";
-var userChoices = {};
+var userChoices = [];
 
 
-// Function to generate random #password input
-  
-
-  // Function to ask for password length 
+// Function to ask for password length 
 function verifyLength() {
-  passwordLength = window.prompt ('Please enter length of password, (must be between 8 and 128 characters)');
+  passwordLength = window.prompt ('Please enter length of password, (must be between 8 and 128 characters.)');
   if (passwordLength < 8 || passwordLength > 128);{
-    alert ('Incorrect character length!\nA number between 8 and 128 must be entered.\nPlease start from the beginning');
+    alert ('Incorrect character length!\nA number between 8 and 128 must be entered.\nPlease start from the beginning.');
     verifyLength();
   } 
-  return passwordLength;
 }
       
 
-  // Function to ask for lowercase characters
-  function verifyLowerCase() {
-    confirmLowerCase = window.prompt ('Would you like to include lowercase characters?\nYes or No');
+// Function to ask for lowercase characters
+function verifyLowerCase() {
+  confirmLowerCase = window.prompt ('Would you like to include lowercase characters?\nYes or No');
       
   if (confirmLowerCase === "yes, y"){
     confirmLowerCase = true;
-    return confirmLowerCase;
-  }
+    }
 
   else if (confirmLowerCase === "no, n"){
     confirmLowerCase = false;
-    return confirmLowerCase;
-  }
+    }
 
   else {
     alert ('Please answer Yes or No');
     verifyLowerCase();
-  }
-    return confirmLowerCase;
-  }
+    }
+}
 
-  // Function to ask for uppercase characters
-  function verifyUpperCase (){
-    confirmUpperCase = window.prompt ('Would you like to include uppercase characters?\nYes or No');
+// Function to ask for uppercase characters
+function verifyUpperCase(){
+  confirmUpperCase = window.prompt ('Would you like to include uppercase characters?\nYes or No');
       
   if (confirmUpperCase === "yes, y"){
-    confirmUpperCase = true;
-    return confirmUpperCase;
-  }
+      confirmUpperCase = true;
+    }
 
   else if (confirmUpperCase === "no, n"){
-    confirmUpperCase = false;
-    return confirmUpperCase;
-  }
+      confirmUpperCase = false;
+    }
 
   else {
-    alert ('Please answer Yes or No');
-    verifyUpperCase();
-  }
-    return confirmUpperCase;
-  }
+      alert ('Please answer Yes or No');
+      verifyUpperCase();
+    }
+}
 
-   // Function to ask for special characters
-   function verifySpecialChar (){
-    confirmSpecialChar = window.prompt ('Would you like to include special characters?\nYes or No');
+// Function to ask for special characters
+function verifySpecialChar (){
+  confirmSpecialChar = window.prompt ('Would you like to include special characters?\nYes or No');
       
   if (confirmSpecialChar === "yes, y"){
     confirmSpecialChar = true;
-    return confirmSpecialChar;
-  }
+    }
 
   else if (confirmSpecialChar === "no, n"){
     confirmSpecialChar = false;
-    return confirmSpecialChar;
-  }
+    }
 
   else {
     alert ('Please answer Yes or No');
     verifySpecialChar();
-  }
-    return confirmSpecialChar;
-  }
+    }
+}
 
-  // Function to ask for numbers characters
-  function verifyNumbersChar (){
-    confirmNumbersChar = window.prompt ('Would you like to include number characters?\nYes or No');
+// Function to ask for numbers characters
+function verifyNumbersChar (){
+  confirmNumbersChar = window.prompt ('Would you like to include number characters?\nYes or No');
       
   if (confirmNumbersChar === "yes, y"){
     confirmNumbersChar = true;
@@ -110,11 +96,11 @@ function verifyLength() {
     verifyNumbersChar();
   }
     return confirmNumbersChar;
-  }
+}
 
   // Function to collate all previous results and generate a random password
 function allChars(){
-  verifyLength ();
+  verifyLength();
   console.log(passwordLength);
   verifyLowerCase();
   console.log(confirmLowerCase);
@@ -127,24 +113,50 @@ function allChars(){
 }
 
 // For loop function used to generate password
-function writePassword (){
+function writePassword(){
   let password = '';
-  for(var i = 0; i < passwordLength; i++){
-  password += userChoices.charAt(Math.floor(Math.random() * userChoices.length)); 
+    for(var i = 0; i < passwordLength; i++){
+    password += userChoices[Math.floor(Math.random() * userChoices.length)]; 
 }
+console.log(password)
 return password;
 }
 
-// Function to connect create Password to querySelector
+// Function to list allChars and writePassword
+function generatePassword() {
+  allChars()
+  if (confirmLowerCase) {
+    userChoices = userChoices.concat(lowerCase)
+  }
 
-function createPassword (){
+  if (confirmUpperCase) {
+    userChoices = userChoices.concat(upperCase)
+  }
+
+  if (confirmSpecialChar) {
+    userChoices = userChoices.concat(special)
+  }
+
+  if (confirmNumbersChar) {
+    userChoices = userChoices.concat(numbers)
+  }
+
+  if (passwordLength) {
+    userChoices = userChoices.concat(passwordLength)
+  }
+
+  return writePassword()
+}
+
+// Function to connect create Password to querySelector
+function createPassword () {
   var newpassword = "";
-  newpassword = characters ();
+  newpassword = generatePassword();
   var passwordtext = documnet.querySelector ('#password');
   passwordtext.value = newpassword;
 
-
-  // Event Listener button
-  generateBtn.addEventListener("Click", createPassword);
-
 }
+
+ // Event Listener button
+var generateBtn = document.querySelector('#generate');
+generateBtn.addEventListener("click", createPassword);
